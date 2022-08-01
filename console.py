7347849f5789f94238a1f3bfb128721d14bce121
odelolajosh <oladapoodelola@gmail.com>
2022-08-02 00:25:23 +0100

    chore: initial project files

---
+++ console.py
@@ -0,0 +1,13 @@
+"""
+The console module provides the `HBNBCommand` class for AirBnb
+command intepreter.
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class defines `AirBnb` line-oriented interpreter"""
+    pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+"""
+This module provides the `BaseModel` class.
+"""
+
+class BaseModel:
+    """Define the `BaseModel` Class
+    The `BaseModel` class defines all common attributes/methods for other classes
+    """
+    pass
