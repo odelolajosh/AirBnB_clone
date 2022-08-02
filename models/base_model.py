3a18bda73e5d4b3dcfc42bd87b6e5a109d35e461
odelolajosh <oladapoodelola@gmail.com>
2022-08-02 21:13:15 +0100

    feature: add task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,35 @@
+#!/usr/bin/python3
 """
 This module provides the `BaseModel` class.
 """
 
+from datetime import datetime
+import uuid
+
+
 class BaseModel:
     """Define the `BaseModel` Class
-    The `BaseModel` class defines all common attributes/methods for other classes
+    The `BaseModel` class defines all common attributes/methods
+    for other classes
     """
-    pass
+    def __init__(self):
+        """"Initialize attribute for `BaseModel` class"""
+        self.id = str(uuid.uuid4())
+        self.updated_at = self.created_at = datetime.now()
+
+    def __str__(self):
+        """Return the string representation of the class"""
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
+
+    def save(self):
+        """Persists instance"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance"""
+        ins = self.__dict__.copy()
+        ins["__class__"] = self.__class__.__name__
+        ins["created_at"] = datetime.isoformat(ins["created_at"])
+        ins["updated_at"] = datetime.isoformat(ins["updated_at"])
+        return ins
