b79ec5b0eb292f1cbbb8c731e69a58d1c4c4178d
odelolajosh <oladapoodelola@gmail.com>
2022-08-03 00:45:28 +0100

    feature: add task 6

--- console.py
+++ console.py
@@ -6,9 +6,24 @@ command intepreter.
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class defines `AirBnb` line-oriented interpreter"""
-    pass
+    intro = ""
+    prompt: str = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Quit the console session """
+        return True
+
+    def do_EOF(self, arg):
+        """ Quits the console session """
+        return True
+
+    def emptyline(self):
+        """ Respond to empty line command """
+        return False
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
