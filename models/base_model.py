b73677f097e4dd38f0d1e02fb09b73a893b980d3
odelolajosh <oladapoodelola@gmail.com>
2022-08-08 09:50:54 +0100

    fix: use utc timing

--- models/base_model.py
+++ models/base_model.py
@@ -24,8 +24,8 @@ class BaseModel:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             storage.new(self)
 
     def __str__(self):
@@ -36,7 +36,7 @@ class BaseModel:
     def save(self):
         """Persists instance"""
         storage.save()
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the instance"""
